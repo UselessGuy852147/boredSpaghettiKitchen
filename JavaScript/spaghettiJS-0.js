564d3fca4fd812b901e61d6b85f383dd98640ed4
UselessGuy852147 <wblackghostwpc212@gmail.com>
2025-08-03 21:34:02 +0300

    Summoned spaghettiJS-0.js

---
+++ JavaScript/spaghettiJS-0.js
@@ -0,0 +1,50 @@
+let Num_____One = '2'
+let nUm___________Teo = '3'
+let cameraShake = 10
+let backGroundRGB = ['red','green','blue']
+let money = 'Money'
+let shit = NaN
+let possibleNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101]
+let operator = '+'
+let Potato = 'cool'
+let Fr__ch = 'Bad' // /j(no /j haha I tricked you(probably not lmao, well it is correct tho /j.))
+let thot makeThis50LinesOfCode
+
+for (let i = 0; i < 1; i++) {
+    let sundotDiameter = '1391400'//km
+    let sundotDiameterInFreedomMeasurments = '864575.8769'//freedom measurments
+    if (Num_____One === '2' && operator === '+' && nUm___________Teo === '3' && Fr__ch !== 'Good' && sundotDiameter === '1391400' && sundotDiameterInFreedomMeasurments === '864575.8769' && !possibleNumbers.includes(34)) {
+        for (let Ture = 34; Ture < 69; Ture+=35) { //Ture is not a typo
+            let True = false
+            let False = false
+            let infinity = 1
+            let infinityPlusOne = 2
+            if (True === true && False === false) {
+                True = false
+            } else if (infinityPlusOne > infinity) {
+                    True = false
+                    False = true
+                    for (let a = 0; a < 15; a+=15) {
+                        let Everything = 'everything'
+                        let Nothing = 0
+                        if (Everything !== 'Nothing') {
+                            Nothing = 1
+                            if (Nothing === 1) {
+                                for (let YAmIDoingThis = 68; YAmIDoingThis < 69; YAmIDoingThis++) {
+                                result = 1
+                                document.body.style.backgroundColor = backGroundRGB[1]
+                                console.log(result)
+                                setTimeout(function() {
+                                    alert('yeppie')
+                                    alert(result)
+                                }, 500);
+                                }
+                            }
+                        } else if (document.body.style.backgroundColor == 'white') {
+                            Everything = 'Everything'
+                        }
+                    }
+                }
+            }
+        }
+     lmao, 
