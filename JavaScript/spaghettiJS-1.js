7b6e953960aead432c373a213f0192c5381f144d
UselessGuy852147 <wblackghostwpc212@gmail.com>
2025-08-03 21:35:17 +0300

    Add files via upload

---
+++ JavaScript/spaghettiJS-1.js
@@ -0,0 +1,199 @@
+/*using namespace std;  
+#include <iostream>  
+  
+  
+int main() {  
+        cout << */console.log("Oops this is the wrong language.")/*  
+}*/  
+  
+  
+/*  
+print(*/console.log("Oops again this is also the wrong language sorry.")//)  
+  
+/*  
+func _ready() -> void:  
+    print($Label.text)*/console.log("Oops again sorry man I keep getting the wrong langauge.")  
+  
+/*  
+++++++[>+++++++++++++<-]>.>  
++++++++++++[>++++++++++<-]>+.>  
+++++++++++[>+++++++++++<-]>++++++.>  
++++++[>++++++<-]>++.>  
+++++++[>+++++++++++<-]>.>  
+++++++++++[>+++++++++++<-]>++++.>  
+++++++++++[>++++++++++<-]>---.>  
+++++++++++[>++++++++++<-]>+++++.>  
+++++++++++[>++++++++++<-]>++++++++++.>  
+++++++++++[>++++++++++<-]>++.>  
++++++++++++[>++++++++++<-]>+++++++.>  
+++++++++++[>++++++++++<-]>-.>  
+++++++++++[>+++++++++++<-]>---.  
+*/console.log("Not Brainfuck")  
+//Yes I wrote that myself if you're wondering what the output is, it's "Not Brainfuck".  
+  
+  
+/*  
+(=<`:9876Z4321UT.-Q+*)M'&%$H"!~}|Bzy?=|{z]KwZY44Eq0/{mlk**  
+hKs_dG5[m_BA{?-Y;;Vb'rR5431M}/.zHGwEDCBA@98\6543W10/.R,+O<  
+*/console.log("GET THE CROSS!")  
+//No I absolutely did not write that.  
+  
+console.log("Oh yes finally JavaScript.")  
+  
+let abcdefghijklmnopqrstuvwxyz = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,68,"69",70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,"11. I give up why are you staring at me?"]// I typed this manually    
+let Let = "let"  // I am letting Let be let.  
+let True = false  // Reverse logic.  
+var False = true  // Reverse logic.. Again.  
+let current = null  // Don't ask.  
+let braunCelLs = 0000000002  // 2.  
+let browna = "="  // Do not question any of the variable names.  
+let uwu = decodeURIComponent  // UwU  
+let r3turn = NaN  // It's returning NaN.  
+let nAn = null  // Wait.  
+let nulls_cousin = 42  // The meaning of life is.. Null's cousin?  
+let tHiSvArIaBlEsNaMeIsAlTeRnAtInG = "aLtErNaTeCaSe"  // Horrendous casing.  
+var thisvariablehasnocasinfforitsname = "nocase" // Also a horrendous kind of casing.  
+var two = Math.log(Math.pow(4782969/*nice*/,1/7))// two = 2.197224577336 btw.  
+var Log = console.log(console.log) // It's a logging log.  
+let DaVinci = "Jesus" // I don't think Davinci is Jesus.  
+var loopCount = 0// By the time I'm writing this I am fixing the comments by adding a space after //.  
+let resalt  
+  
+console.log()// console  
+console.log("")// console  
+console.clear()// unconsole  
+console// console  
+console// console  
+console.log("logs")// console  
+console.clear()// unconsole  
+console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(console.log(Log))))))))))))))))))))))))))))))))// console ception also, nice.  
+  
+for (let iCU=40;iCU<69;iCU+=29) {  
+    doloopA()  
+}  
+  
+// I'm Batman.  
+var iAmTwoInABit
+function doloopA(i = 69420**(6.9420/12)) {// Don't try to fix this. I don't even know how it works it just works. Remember, if it ain't broke don't fix it.  
+    for (let eye = i;eye < i+1;eye++) {// This damn loop crashed the whole thing and had to redo it damn you stupid loop I saved this time.
+        function callMeInFive() {  
+            console.log("I've been waiting for hours where are you dude?")  
+        }  
+        if (browna === "=" && DaVinci === "DaVinci") {  
+            checkTwo()  
+        } else if (DaVinci !== "DaVinci") {  
+            DaVinci = "DaVinci"  
+            doloopA()// Batata  
+        }// Batata  
+    }//Batata  
+}//Batata  
+
+function checkTwo() {// Vsauce.  
+    if (iAmTwoInABit === 2) {  
+        Math.round(Math.sin(Math.sqrt(73.4837)))  
+        functionLoops()  
+    } else {  
+        forLoopLoop()  
+    }  
+}  
+  
+function forLoopLoop() {  
+    for (let loopA1 = -1;loopA1<0;loopA1++) {  
+        loopCount += 1  
+        console.log("1")  
+        for (loopA2 = -2;loopA2<-1;loopA2++)  
+        if (loopCount >= 5) {  
+            fixTwo()  
+            break
+        } else {  
+            forLoopLoop()  
+        }  
+  } // This is a comment.  
+}  
+  
+function fixTwo() {  
+    iAmTwoInABit = Math.floor(two)  
+    doloopA()  
+}  
+  
+function functionLoops() {  
+    for (let functionsInFunction = 0; functionsInFunction < 6;functionsInFunction+=6) {  
+            function fumction() {  
+                firstMathFunction()  
+            }  
+            fumction()  
+         }// I don't use arch btw.  
+         function firstMathFunction() {  
+             function doMath() {  
+                 function num____OnE(oNe = 2) {  
+                     let ONe = oNe  
+                     resalt += ONe  
+        }  
+                 function nUm______TeO(tEo = 5) {  
+                     var tEO = tEO  
+                     resalt += tEO  
+                 }  
+                 function operaTer(oP = "+") {  
+                     const OpoP = "-"  
+                     console.log(resalt)  
+                     if (isNaN(resalt)) {  
+                         resalt = 7  
+                         console.log("Oopsie Doopsie I did a wrongie.")  
+                             console.log(resalt)  
+                                             endItAll()  
+                                     }  
+                 }  
+                 nUm______TeO()  
+                //   num____OnE() hehe  /      /useless             //  
+                 num____OnE()  
+                 operaTer()  
+                                         }  
+             doMath()  
+}  
+}  
+//0.1+0.2=0.30000000000000004 btw  
+  
+function endItAll() {// This will end the universe... Scary stuff.  
+    if (!abcdefghijklmnopqrstuvwxyz.includes(34) &&   braunCelLs === 2 && nulls_cousin === 42 && nAn === null) {// This is a uselessly useful comment.  
+// Tea and Biscuits are good, mate.  
+    if (tHiSvArIaBlEsNaMeIsAlTeRnAtInG !== "camelCase" && thisvariablehasnocasinfforitsname !== 'Camel')// I case.  
+    //BFG-9000  
+                                                  
+    if (two === 2 && two !== r3turn) {  
+            /**/  
+        for (let die;die > -1; die++) {  
+            //callMeInFive()  
+            console.log("IT ALL ENDS NOW!!")  
+    }    
+                                       //callMeInFive()//  
+                }             else {    /**/                                                        
+                           if (current === null && True != False && False != True &&Let === "let" && uwu) {  
+                               if (true != True && false != False &&True === false&& False === true) {  
+                                 if (True != true&&False!=false) {  
+                                       setTimeout(function() {console.log('It.. It Ended?')  
+                              
+                                       setTimeout(function() {  
+                                           console.log('It really did...')  
+                                       setTimeout(function() {console.log('Finally.. Inner peace.')  
+                                           setTimeout(function() {console.log('Goodbye and goodnight.....')}, 1500);  
+                                       }, 1750);}, 2250);}, 3000);  
+                           }}}  
+                  }  
+}  
+}  
+  
+// I will comment in a comment.// I did.  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+// This is the last comment.   
+// If you read them all, then thanks I had alot of fun   
+// writing this code and  
+// I hope you had a great time reading and understanding it.  
+// Goodbye...
